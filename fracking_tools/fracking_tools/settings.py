36cfcf8ee53df4e6b48e8f7f43b406dd073bd8cd
Rudy Huezo <rahuezo@ucdavis.edu>
2019-05-07 09:00:40 -0700

    Updated settings.

--- fracking_tools/fracking_tools/settings.py
+++ fracking_tools/fracking_tools/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=z5_vhi&uo(3!8r6k7izl1n!(vp5l$hq@))dhkhnltihzo2*+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.22.245.213']
 
