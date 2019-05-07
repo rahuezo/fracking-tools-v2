0b70614aa33c9257d082247cb686e90e7ca7ceb7
Rudy Huezo <rahuezo@ucdavis.edu>
2019-05-07 08:26:11 -0700

    Postgres DB settings updated.

--- fracking_tools/fracking_tools/settings.py
+++ fracking_tools/fracking_tools/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'fracking_tools.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'fracking_tools',
+            'USER': 'rudy',
+            'PASSWORD': '23560208',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
