91865362f816df1beb02465e38ff008a7ab2f3f5
Rudy Huezo <rahuezo@ucdavis.edu>
2019-05-10 09:21:45 -0700

    Changed STATIC_ROOT

--- fracking_tools/fracking_tools/settings.py
+++ fracking_tools/fracking_tools/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_serve')
 
 
 STATICFILES_DIRS = [
