c8d141d0c073fafb60e31026eac4f9355d2d8fc1
Rudy Huezo <rahuezo@ucdavis.edu>
2019-05-10 09:10:32 -0700

    Changed static

--- fracking_tools/fracking_tools/settings.py
+++ fracking_tools/fracking_tools/settings.py
@@ -138,7 +138,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_remote')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = "/var/www/fracking_tools/static/"
+# STATIC_ROOT = "/home/rudy/fracking-tools-v2/env/local/lib/python2.7/site-packages/django/contrib/admin/static"
 
 
 STATICFILES_DIRS = [
