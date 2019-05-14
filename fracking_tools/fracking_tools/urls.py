f29b17261ca11cefd7c9ebe46d8046f2caddc6cb
Rudy Huezo <rahuezo@ucdavis.edu>
2019-05-14 09:01:55 -0700

    Update urls.py

--- fracking_tools/fracking_tools/urls.py
+++ fracking_tools/fracking_tools/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     url(r'^documentation/', include('documentation.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
